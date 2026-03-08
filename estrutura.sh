2e34f2750731251c604c182ca994a82d93306eaf
zainer56-dot <zainer56@gmail.com>
2026-03-08 12:38:23 -0300

    Create estrutura.sh

---
+++ estrutura.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#
+# SCRIPT: NOME_DO_SCRIPT
+# AUTHOR: NOME_DO_AUTOR
+# DATE: DATA_DE_CRIACAO
+# REV: 1.1.A (Valores válidos: A, B, D, T e P)
+# (Para Alpha, Beta, Dev, Test e Produção)
+#
+# PLATFORM: (ESPECIFICAR: AIX, HP-UX, Linux, OpenBSD, Solaris
+# ou Não dependente de plataforma)
+#
+# PURPOSE: Dar uma descrição clara, e se necessário longa,
+# do propósito do script. Isso também ajuda você a manter
+# o foco na tarefa em questão.
+#
+# REV LIST:
+# DATE: DATA_DA_REVISAO
+# BY: AUTOR_DA_MODIFICACAO
+# MODIFICATION: Descreva o que foi modificado,
+# novos recursos, etc.
+#
+#
+# set -n   # Descomente para verificar a sintaxe do script, sem #executar.
+#          # ATENÇÃO: não esqueça de comentar novamente,
+#          # senão o script não será executado!
+# set -x   # Descomente para depurar este script.
+#
+##########################################################
+# DEFINIR ARQUIVOS E VARIÁVEIS AQUI
+##########################################################
+
+##########################################################
+# DEFINIR FUNÇÕES AQUI
+##########################################################
+
+##########################################################
+# INÍCIO DO MAIN
+##########################################################
+
+# Fim do script
+
